4669822a1a04c15e123f90267476f19b9509d764
Alfredo Sánchez <45853187+AlfredoSV@users.noreply.github.com>
2023-09-10 19:44:13 -0600

    Add project files.

---
+++ CSharp_ADVANCED/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+	/*
+	Delegates
+	Func, Action y Predicate
+	*/
+	public delegate void PrintMessage(string value);
+
+	public static void Main(string[] args)
+	{
+		PrintMessage printHello = new PrintMessage(PrintHello);
+
+		printHello("Alfredo");
+
+		Console.Read();
+	}
+
+	public static void PrintHello(string name)
+	{
+		Console.WriteLine($"Hola, {name}.");
+	}
+}
+
